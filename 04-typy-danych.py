55cd6097c76e0c8ef8caf2191e5378c17e053fe3
Bruno Żychowski <91904672+rzyhu@users.noreply.github.com>
2023-06-14 14:21:26 +0200

    Add files via upload

---
+++ 04-typy-danych.py
@@ -0,0 +1,31 @@
+#name = "Bruno"
+#print(name)
+
+#a = 10
+#b = 2.5
+#print(a+b)
+
+#a = 10
+#a = 100
+#a = 1000
+#a = 10000
+#a = 100000
+#a = 1_000_000
+#print(a)
+
+#is_sky_blue = True
+#is_sun_blue = False
+
+#print(is_sky_blue)
+#print(is_sun_blue)
+
+#print(type(name))
+#print(type(is_sky_blue))
+#print(type(a))
+
+#first = 2
+#second = "3"
+#print(first+int(second))
+
+name = "Bruno"
+print(int(name))
